8eed7da99ba7f324dbe4384509b876783578d660
luisgs7 <luisgsilva270@gmail.com>
2024-03-30 15:46:05 -0300

    refactor App component.

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from "react"
+import React, { Component } from "react"
 import { StyleSheet, Text, View } from "react-native"
+import Button from "./src/components/Button"
 
 
-export default props = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={style.text}>Calcular App</Text>
-    </View>
-  )
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttons}>
+          <Button label='AC'/>
+          <Button label='/'/>
+          <Button label='7'/>
+          <Button label='8'/>
+          <Button label='9'/>
+          <Button label='*'/>
+          <Button label='4'/>
+          <Button label='5'/>
+          <Button label='6'/>
+          <Button label='-'/>
+          <Button label='1'/>
+          <Button label='2'/>
+          <Button label='3'/>
+          <Button label='+'/>
+          <Button label='0'/>
+          <Button label='.'/>
+          <Button label='='/>
+        </View>
+      </View>
+    )
+  }
 }
 
-const style = StyleSheet.create({
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-    fontWeight: 'bold',
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   }
 })
